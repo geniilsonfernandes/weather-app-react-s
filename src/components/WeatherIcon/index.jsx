8403dbb17bc701c14b3c783116be4e2baa8dd193
Genilson <geniilsonfernandes@gmail.com>
2022-06-21 18:17:24 -0300

    :wrench: refactor(WeatherIcon): add prop size

--- src/components/WeatherIcon/index.jsx
+++ src/components/WeatherIcon/index.jsx
@@ -13,53 +13,53 @@ import {
   Compass,
 } from "./Icons";
 
-export const WeatherIcon = ({ icon, label }) => {
+export const WeatherIcon = ({ icon, label, size }) => {
   switch (icon) {
     case "ClearSky":
       return (
-        <S.Icon aria-label={label}>
+        <S.Icon aria-label={label} size={size}>
           <ClearSky />
         </S.Icon>
       );
     case "BrokenClouds":
       return (
-        <S.Icon aria-label={label}>
+        <S.Icon aria-label={label} size={size}>
           <BrokenClouds />
         </S.Icon>
       );
     case "FewClouds":
       return (
-        <S.Icon aria-label={label}>
+        <S.Icon aria-label={label} size={size}>
           <FewClouds />
         </S.Icon>
       );
     case "Mist":
       return (
-        <S.Icon aria-label={label}>
+        <S.Icon aria-label={label} size={size}>
           <Mist />
         </S.Icon>
       );
     case "Rain":
       return (
-        <S.Icon aria-label={label}>
+        <S.Icon aria-label={label} size={size}>
           <Rain />
         </S.Icon>
       );
     case "ScatteredClouds":
       return (
-        <S.Icon aria-label={label}>
+        <S.Icon aria-label={label} size={size}>
           <ScatteredClouds />
         </S.Icon>
       );
     case "Snow":
       return (
-        <S.Icon aria-label={label}>
+        <S.Icon aria-label={label} size={size}>
           <Snow />
         </S.Icon>
       );
     case "Thunderstorm":
       return (
-        <S.Icon aria-label={label}>
+        <S.Icon aria-label={label} size={size}>
           <Thunderstorm />
         </S.Icon>
       );
@@ -86,4 +86,5 @@ const cases = [
 WeatherIcon.propTypes = {
   icon: P.oneOf(cases),
   label: P.oneOf(cases),
+  size: P.number,
 };
--- src/components/WeatherIcon/styles.js
+++ src/components/WeatherIcon/styles.js
@@ -4,4 +4,9 @@ export const Icon = styled.span`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  svg {
+    width: ${({ size }) => size + "px"};
+    height: ${({ size }) => size + "px"};
+  }
 `;
