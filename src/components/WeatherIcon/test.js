03da4117fc76bc607ad45de3b70f67be5f4818cb
Genilson <geniilsonfernandes@gmail.com>
2022-06-21 18:23:08 -0300

    :heavy_check_mark:test(WeatherIcon): Update test

--- src/components/WeatherIcon/test.js
+++ src/components/WeatherIcon/test.js
@@ -21,4 +21,10 @@ describe("<WeatherIcon />", () => {
     render(<WeatherIcon />);
     expect(screen.getByLabelText("Not found")).toBeInTheDocument();
   });
+  it("shoud render icon with size pass", () => {
+    render(<WeatherIcon icon="ClearSky" label="ClearSky" size="30" />);
+    expect(screen.getByLabelText("ClearSky").firstChild).toHaveStyle({
+      width: "30px",
+    });
+  });
 });
