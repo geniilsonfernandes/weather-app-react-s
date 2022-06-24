d0dc848143eba9a73be1d289772ab7a922a04af0
Genilson <geniilsonfernandes@gmail.com>
2022-06-24 13:23:09 -0300

    :wrench:refactor(theme): add new colors scheme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -6,6 +6,28 @@ export default {
     black200: "#252525",
     black300: "#343434",
     black500: "#444444",
+    black: {
+      100: "#111111",
+      200: "#252525",
+      300: "#333333",
+      400: "#4D4D4D",
+      500: "#666666",
+    },
+    white: {
+      100: "#fffFFF",
+      200: "#E6E6E6",
+      300: "#CCCCCC",
+      400: "#B3B3B3",
+      500: "#999999",
+    },
+  },
+  fontSizes: {
+    xsmall: "14rem",
+    normal: "16rem",
+    medium: "18rem",
+    large: "26rem",
+    xlarge: "42rem",
+    biger: "52rem",
   },
   radius: "4px",
 };
