f33ec091bd68a9cdb7f7420b7a4c5ab706f00863
Genilson <geniilsonfernandes@gmail.com>
2022-06-25 20:49:23 -0300

    :heavy_check_mark:test(Base): Add renderWithTheme

--- src/pages/Base/Base.test.js
+++ src/pages/Base/Base.test.js
@@ -1,9 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import renderWithTheme from "../../utils/test/renderWithTheme";
+import { screen } from "@testing-library/react";
 import { Base } from ".";
 
 describe("<Base />", () => {
   it("should render base with children ", () => {
-    render(
+    renderWithTheme(
       <Base>
         <div data-testid="home"></div>
       </Base>
