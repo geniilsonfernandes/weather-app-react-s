5849d13b3fcd4a01aad9fc8f2b562521bacfd36b
Genilson <geniilsonfernandes@gmail.com>
2022-06-26 18:58:34 -0300

    :heavy_check_mark:test(Base): update test

--- src/pages/Base/Base.test.js
+++ src/pages/Base/Base.test.js
@@ -2,6 +2,17 @@ import renderWithTheme from "../../utils/test/renderWithTheme";
 import { screen } from "@testing-library/react";
 import { Base } from ".";
 
+jest.mock("../../context/dataContext", () => {
+  return {
+    useDataContext: () => ({
+      fetchQuery: jest.fn(),
+      loading: false,
+      error: null,
+      sucess: false,
+    }),
+  };
+});
+
 describe("<Base />", () => {
   it("should render base with children ", () => {
     renderWithTheme(
