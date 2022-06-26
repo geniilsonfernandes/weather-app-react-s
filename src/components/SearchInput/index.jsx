253022c0a5156be6b7666a9d861cd6b8eae1ace5
Genilson <geniilsonfernandes@gmail.com>
2022-06-26 00:50:50 -0300

    :zap:chore(SearchInput): add new styles for cases error

--- src/components/SearchInput/index.jsx
+++ src/components/SearchInput/index.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import P from "prop-types";
 import { FiSearch } from "react-icons/fi";
 
 import * as S from "./styles.js";
 
-export const SearchInput = ({ onSubmit }) => {
+export const SearchInput = ({ onSubmit, error, loading, sucess }) => {
   const [value, setValue] = useState("");
 
   const handleChangeValue = (target) => {
@@ -14,11 +14,14 @@ export const SearchInput = ({ onSubmit }) => {
     e.preventDefault();
     !!value && onSubmit(value);
   };
+  useEffect(() => {
+    sucess ? setValue("") : setValue((prev) => prev);
+  }, [sucess]);
 
   return (
-    <S.Wrapper onSubmit={handleSubmit}>
+    <S.Wrapper onSubmit={handleSubmit} error={error} $loading={loading}>
       <S.WrapperIcon htmlFor="search">
-        <FiSearch aria-label="search icon button" color="#DDDDDD" size={18} />
+        <FiSearch aria-label="search icon button" size={18} />
       </S.WrapperIcon>
       <S.SearchInput
         value={value}
@@ -26,11 +29,17 @@ export const SearchInput = ({ onSubmit }) => {
         onChange={({ target }) => handleChangeValue(target)}
         type="text"
         id="search"
+        disabled={loading}
       />
+      {!!error && <S.Error>{error}</S.Error>}
+      {!!loading && <S.DotsLoading aria-label="seeding"></S.DotsLoading>}
     </S.Wrapper>
   );
 };
 
 SearchInput.propTypes = {
   onSubmit: P.func,
+  error: P.string,
+  loading: P.bool,
+  sucess: P.bool,
 };
--- src/components/SearchInput/styles.js
+++ src/components/SearchInput/styles.js
@@ -1,22 +1,69 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const wrapperModifer = {
+  error: (theme) => css`
+    box-shadow: 0 0 0 1px ${theme.colors.error};
+    animation: shake-horizontal 0.3s ease-in-out both;
+    &:focus-within {
+      box-shadow: 0 0 0 1px ${theme.colors.error};
+    }
+    @keyframes shake-horizontal {
+      0%,
+      100% {
+        transform: translateX(0);
+      }
+      20%,
+      50%,
+      60% {
+        transform: translateX(-4px);
+      }
+      40%,
+      80% {
+        transform: translateX(4px);
+      }
+    }
+    ${WrapperIcon} {
+      svg {
+        color: ${theme.colors.error} !important;
+      }
+    }
+  `,
+  loading: (theme) => css`
+    background: ${theme.colors.black[200]};
+    opacity: 0.6;
+    cursor: not-allowed;
+    &:focus-within {
+      box-shadow: 0 0 0 1px ${({ theme }) => theme.colors.black[300]};
+    }
+  `,
+};
 
 export const Wrapper = styled.form`
-  height: 52px;
-  background: ${({ theme }) => theme.colors.black[300]};
-  width: 100%;
-  max-width: 500px;
-  display: flex;
-  border-radius: 4px;
-  &:focus-within {
-    box-shadow: 0 0 0 1px ${({ theme }) => theme.colors.white[100]};
-  }
+  ${({ error, loading, theme }) => css`
+    background: ${theme.colors.black[300]};
+    height: 52px;
+    width: 100%;
+    max-width: 500px;
+    display: flex;
+    border-radius: 4px;
+    position: relative;
+    &:focus-within {
+      box-shadow: 0 0 0 1px ${({ theme }) => theme.colors.white[100]};
+    }
+    ${!!error && wrapperModifer.error(theme)}
+    ${!!loading && wrapperModifer.loading(theme)}
+  `}
 `;
+
 export const WrapperIcon = styled.label`
   padding: 0 14px;
   display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
+  ${({ theme }) => css`
+    color: ${theme.colors.white[100]};
+  `}
 `;
 
 export const SearchInput = styled.input`
@@ -29,5 +76,71 @@ export const SearchInput = styled.input`
   font-size: ${({ theme }) => theme.fontSizes.normal};
   &::placeholder {
     color: ${({ theme }) => theme.colors.white[400]};
+  }
+  &:disabled {
+    cursor: not-allowed;
+  }
+`;
+
+export const Error = styled.span`
+  position: absolute;
+  bottom: -30px;
+  ${({ theme }) => css`
+    font-size: ${theme.fontSizes.xsmall};
+    color: ${theme.colors.error};
+  `}
+`;
+
+export const DotsLoading = styled.span`
+  position: absolute;
+  right: 34px;
+  top: 22px;
+
+  width: 8px;
+  height: 8px;
+  border-radius: 5px;
+  background-color: ${({ theme }) => theme.colors.white[300]};
+  color: ${({ theme }) => theme.colors.white[300]};
+  animation: dotFlashing 1s infinite linear alternate;
+  animation-delay: 0.5s;
+
+  &::before,
+  &::after {
+    content: "";
+    display: inline-block;
+    position: absolute;
+    top: 0;
+  }
+
+  &::before {
+    left: -15px;
+    width: 8px;
+    height: 8px;
+    border-radius: 5px;
+    background-color: ${({ theme }) => theme.colors.white[300]};
+    color: ${({ theme }) => theme.colors.white[300]};
+    animation: dotFlashing 1s infinite alternate;
+    animation-delay: 0s;
+  }
+
+  &::after {
+    left: 15px;
+    width: 8px;
+    height: 8px;
+    border-radius: 5px;
+    background-color: ${({ theme }) => theme.colors.white[300]};
+    color: ${({ theme }) => theme.colors.white[300]};
+    animation: dotFlashing 1s infinite alternate;
+    animation-delay: 1s;
+  }
+
+  @keyframes dotFlashing {
+    0% {
+      background-color: ${({ theme }) => theme.colors.white[300]};
+    }
+    50%,
+    100% {
+      background-color: ${({ theme }) => theme.colors.black[500]};
+    }
   }
 `;
