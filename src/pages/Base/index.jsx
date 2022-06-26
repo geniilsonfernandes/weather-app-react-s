8e8d16b75c134cd0123bce4b115bd565c0aa93da
Genilson <geniilsonfernandes@gmail.com>
2022-06-26 18:58:15 -0300

    :zap:chore(Base):update props for input and add context

--- src/pages/Base/index.jsx
+++ src/pages/Base/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import P from "prop-types";
 import { SearchInput } from "../../components/SearchInput";
 import * as S from "./styles";
@@ -7,7 +7,7 @@ import { Container } from "../../components/Layout";
 import { useDataContext } from "../../context/dataContext";
 
 export const Base = ({ children }) => {
-  const { fetchQuery, loading, error } = useDataContext();
+  const { fetchQuery, loading, error, sucess } = useDataContext();
   const handleSubmit = (query) => {
     fetchQuery(query);
   };
@@ -20,10 +20,10 @@ export const Base = ({ children }) => {
             onSubmit={handleSubmit}
             error={error}
             loading={loading}
+            sucess={sucess}
           />
         </S.Header>
       </Container>
-
       {children}
     </S.Main>
   );
