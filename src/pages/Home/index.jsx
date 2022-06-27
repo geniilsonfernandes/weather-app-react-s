3a198fd6fa76d24f9fb63a65a8a980f949a4aa50
Genilson <geniilsonfernandes@gmail.com>
2022-06-27 20:51:05 -0300

    :zap:chore(Home):populate data

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,11 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { WiStrongWind } from "react-icons/wi";
+import React, { useEffect } from "react";
+import {
+  WiStrongWind,
+  WiRaindrop,
+  WiBarometer,
+  WiThermometer,
+} from "react-icons/wi";
 import { MeasureCard } from "../../components/MeasureCard";
 import { Menu } from "../../components/Menu";
 import { Weather } from "../../components/Weather";
@@ -52,44 +57,57 @@ const mockWeatherCard = [
 export const Home = () => {
   const { loading, data, error, state } = useDataContext();
 
-  return (
-    <S.Container>
-      <S.SectionMenu>
-        <Menu />
-      </S.SectionMenu>
-      <S.WeatherWrapper>
-        <Weather {...mockWeather} />
-      </S.WeatherWrapper>
-      <S.BoxWrapper margin="16">
-        <MeasureCard
-          title="Wind"
-          mesure="10km/h"
-          icon={<WiStrongWind aria-label="Wind" />}
-        />
-        <MeasureCard
-          title="Pressure"
-          mesure="963 mbar"
-          icon={<WiStrongWind aria-label="Wind" />}
-        />
-        <MeasureCard
-          title="Humidity"
-          mesure="80%"
-          icon={<WiStrongWind aria-label="Wind" />}
-        />
-        <MeasureCard
-          title="Precipitation"
-          mesure="80%"
-          icon={<WiStrongWind aria-label="Wind" />}
-        />
-      </S.BoxWrapper>
-      <S.SectionWrapper>
-        <Heading title="Week" />
-        <S.SectionGrid>
-          {mockWeatherCard.map((item) => (
-            <WeatherCard key={item.temp} {...item} />
-          ))}
-        </S.SectionGrid>
-      </S.SectionWrapper>
-    </S.Container>
-  );
+  if (!data.name) {
+    return <span>no have data</span>;
+  } else {
+    return (
+      <S.Container>
+        <S.SectionMenu>
+          <Menu />
+        </S.SectionMenu>
+        <S.WeatherWrapper>
+          <Weather
+            temp={data.current.temp.toFixed()}
+            condition={data.current.weather[0].main}
+            description={data.current.weather[0].description}
+            date={data.current.dt}
+          />
+        </S.WeatherWrapper>
+        <S.BoxWrapper margin="16">
+          <MeasureCard
+            title="Wind"
+            unit="km/h"
+            mesure={data.current.wind_speed}
+            icon={<WiStrongWind aria-label="Wind" />}
+          />
+          <MeasureCard
+            title="Pressure"
+            unit="mbar"
+            mesure={data.current.pressure}
+            icon={<WiBarometer aria-label="Pressure" />}
+          />
+          <MeasureCard
+            title="Humidity"
+            unit="%"
+            mesure={data.current.humidity}
+            icon={<WiRaindrop aria-label="Humidity" />}
+          />
+          <MeasureCard
+            title="Feels like"
+            unit="°C"
+            mesure={data.current.feels_like}
+            icon={<WiThermometer aria-label="Feels like" />}
+          />
+        </S.BoxWrapper>
+        <S.SectionWrapper>
+          <Heading title="Week" />
+          <S.SectionGrid>
+            {mockWeatherCard.map((item) => (
+              <WeatherCard key={item.temp} {...item} />
+            ))}
+          </S.SectionGrid>
+        </S.SectionWrapper>
+      </S.Container>
+    );
+  }
 };
