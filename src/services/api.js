02593efbc342a9a381dcd1093a5ba17f961e719e
Genilson <geniilsonfernandes@gmail.com>
2022-06-29 15:10:14 -0300

    :heavy_check_mark:test(Api): add test for fetch api used msw mock

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,33 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://api.openweathermap.org/data/2.5/',
+  baseURL: "https://api.openweathermap.org/data/2.5/",
 });
+
+export const getForecast = async (queryValue) => {
+  try {
+    const placeReponse = await api.get("weather", {
+      params: {
+        q: queryValue,
+        lang: "en",
+        appid: process.env.REACT_APP_API_KEY,
+        units: "metric",
+      },
+    });
+    const { coord, name } = placeReponse.data;
+    const forecastReponse = await api.get("onecall", {
+      params: {
+        ...coord,
+        lang: "en",
+        appid: process.env.REACT_APP_API_KEY,
+        units: "metric",
+        exclude: "hourly,minutely",
+      },
+    });
+    return { ...forecastReponse.data, name, coord };
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
+};
 
 export default api;
